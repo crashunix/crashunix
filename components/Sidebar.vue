eb25730bc68f98492dfeb98374a536c626b4c6e4
Juan Gomes Macário <jgomesmacario@gmail.com>
2020-12-07 18:54:52 -0300

    theme switcher

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="fixed lg:sticky px-6 lg:p-0 h-16 lg:h-full w-full lg:w-16 lg:w-32 border-r flex lg:flex-col justify-between transition-colors duration-500 ease-in-out z-10" :class="{white_sidebar: scrollPosition > 50}">
+  <div class="fixed lg:sticky px-6 lg:p-0 h-16 lg:h-full w-full lg:w-16 lg:w-32 border-r flex lg:flex-col justify-between transition-colors duration-500 ease-in-out z-10 dark:bg-blue-200" :class="{white_sidebar: scrollPosition > 50}">
     <div class="h-16 lg:h-32 flex justify-center items-center">
-        <img v-if="type != 'detail'" src="~assets/icons/code.svg" alt="bar-chart-2"/>
+        <object v-if="type != 'detail'" data="https://www.flaticon.com/svg/static/icons/svg/1829/1829070.svg" type="image/svg+xml">
+          CODE
+        </object>
         <a v-else @click="$router.go(-1)" class="cursor-pointer">
           <img class="w-6" src="~assets/icons/arrow-left.svg" alt="Back" />
         </a>
     </div>
-    <div class="h-16 lg:h-32 flex justify-center items-center">
-        <img src="~assets/icons/moon.svg" alt="moon"/>
-    </div>
+    <button @click="toggleTheme('dark-theme')" class="h-16 lg:h-32 flex justify-center items-center">
+      {{theme}}
+        <img v-if="theme == 'light-theme'" src="~assets/icons/moon.svg" alt="moon"/>
+        <img v-else src="~assets/icons/sun.svg" alt="sun"/>
+    </button>
   </div>
 </template>
 <style>
@@ -17,6 +21,9 @@
   }
 </style>
 <script>
+
+import { mapGetters, mapActions } from "vuex";
+
 export default {
   props: ['type'],
   data() {
@@ -28,6 +35,17 @@ export default {
     updateScroll() {
       this.scrollPosition = window.scrollY;
     },
+    ...mapActions('theme', ['setTheme']),
+    toggleTheme() {
+      this.setTheme(this.theme == 'light' ? 'dark' : 'light');
+    }
+  },
+  computed: {
+    ...mapGetters('theme', ['getTheme']),
+    theme() {
+      return this.getTheme;
+      // return this.$store.getters['theme/getTheme']
+    }
   },
   mounted() {
     window.addEventListener('scroll', this.updateScroll);
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="lg:h-screen lg:flex">
+  <div class="lg:h-screen lg:flex" :class="theme">
     <Sidebar />
     <div class="flex-1 h-full overflow-y-auto px-8">
       <Header />
@@ -12,9 +12,16 @@
 </style>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
   data() {
     return {};
   },
+  computed: {
+    ...mapGetters('theme', ['getTheme']),
+    theme() {
+      return this.getTheme;
+    }
+  }
 };
 </script>
